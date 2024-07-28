d7acf28daf8e6a26dc3c423bc8f4f228f2344104
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-07-28 23:04:02 +0300

    Update Easy_Sunday_Excel_Challenge_28th_July.py

--- Easy_Sunday_Excel_Challenge_28th_July.py
+++ Easy_Sunday_Excel_Challenge_28th_July.py
@@ -8,13 +8,10 @@ df = xl("B3:D11", headers=True)
 
 # Perform data munging
 df['ind'] = (df['Student'] != df['Student'].shift(1)).cumsum()
-
 df = df.pivot(
     index=['ind', 'Student'], columns='Test', values='Result'
 ).reset_index().fillna('exempt')
-
 df = df.iloc[:, 1:]
-
 df.columns = [''] + df.columns[1:].tolist()
 
 # Display the final results
